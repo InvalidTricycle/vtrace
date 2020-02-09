35d8f794e5f8f625dea706bd84d6fee48c163d13
InvalidTricycle <46931987+InvalidTricycle@users.noreply.github.com>
2020-02-09 16:27:19 +0800

    Add files via upload

---
+++ cities.js
@@ -0,0 +1,62 @@
+var cities=[
+  {
+    "city": "青岛市",
+    "count": 47
+  },
+  {
+    "city": "济南市",
+    "count": 42
+  },
+  {
+    "city": "临沂市",
+    "count": 40
+  },
+  {
+    "city": "济宁市",
+    "count": 39
+  },
+  {
+    "city": "烟台市",
+    "count": 36
+  },
+  {
+    "city": "威海市",
+    "count": 34
+  },
+  {
+    "city": "潍坊市",
+    "count": 34
+  },
+  {
+    "city": "聊城市",
+    "count": 33
+  },
+  {
+    "city": "德州市",
+    "count": 30
+  },
+  {
+    "city": "泰安市",
+    "count": 23
+  },
+  {
+    "city": "枣庄市",
+    "count": 21
+  },
+  {
+    "city": "淄博市",
+    "count": 16
+  },
+  {
+    "city": "日照市",
+    "count": 15
+  },
+  {
+    "city": "菏泽市",
+    "count": 14
+  },
+  {
+    "city": "滨州市",
+    "count": 11
+  }
+];
